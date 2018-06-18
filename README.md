11eed874a7c6d0cf2d94033a637bfc07b0297d70
David Zhang <crispgm@gmail.com>
2018-06-18 14:30:17 +0800

    Init module

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # go-rugby-dict
+
 Rugby dict lib in Go
+
+## Usage
+
+```shell
+go get github.com/rugby-board/go-rugby-dict
+```
+
+```go
+import (
+    dict "github.com/rugby-board/go-rugby-dict"
+)
+
+chineseTranslation, err := dict.Query("Frans Steyn")
+```
---
+++ main_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	d := NewDict("dict.yaml")
+	err := d.Load()
+	if err != nil {
+		t.Error("Load dict failed")
+	}
+	if d.Size() <= 0 {
+		t.Error("Dict is empty")
+	}
+}
+
+func TestQueries(t *testing.T) {
+	d := NewDict("dict.yaml")
+	err := d.Load()
+	if err != nil {
+		t.Error("Load dict failed")
+	}
+	var trans string
+	trans, err = d.Query("All Blacks")
+	if trans != "全黑" {
+		t.Error("Translate All Black failed")
+	}
+	trans, err = d.Query("Frans Steyn")
+	if trans != "弗朗索瓦·斯特恩" {
+		t.Error("Translate Frans Steyn failed")
+	}
+	trans, err = d.Query("François Steyn")
+	if trans != "弗朗索瓦·斯特恩" {
+		t.Error("Translate François Steyn failed")
+	}
+}
