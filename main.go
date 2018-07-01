78afedc4ea9485002da0f47326932c3b1e9932ae
David Zhang <crispgm@gmail.com>
2018-07-01 19:31:46 +0800

    HTTP Service

--- main.go
+++ main.go
@@ -1,96 +1,32 @@
-package dict
+package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
 
-	yaml "gopkg.in/yaml.v2"
+	"github.com/gin-gonic/gin"
+	"github.com/rugby-board/go-rugby-dict/dict"
 )
 
-// Dict object
-type Dict struct {
-	dictPath string
-	wordList []Word
-	size     int
-}
-
-// Word object
-type Word struct {
-	Origin      string `yaml:"w"`
-	Translation string `yaml:"t"`
-}
-
-const (
-	// RepoPath Relative path of this repo
-	RepoPath = "github.com/rugby-board/go-rugby-dict"
-	// DefaultDictPath of dict.yaml
-	DefaultDictPath = "dict.yaml"
-)
-
-// LoadEnvConfPath ...
-func loadRealPath(yamlPath string) (string, error) {
-	gopath := os.Getenv("GOPATH")
-	if len(gopath) == 0 {
-		return "", errors.New("GOPATH is not set")
-	}
-
-	path := fmt.Sprintf("%s/src/%s/%s", gopath, RepoPath, yamlPath)
-	return path, nil
-}
-
-// NewDict returns a Dict
-func NewDict(dictPath string) *Dict {
-	return &Dict{
-		dictPath: dictPath,
-		size:     0,
-		wordList: make([]Word, 0, 1024),
-	}
-}
-
-// NewDefaultDict returns a default Dict
-func NewDefaultDict() *Dict {
-	return &Dict{
-		dictPath: DefaultDictPath,
-		size:     0,
-		wordList: make([]Word, 0, 1024),
-	}
-}
-
-// Load a dict file
-func (d *Dict) Load() error {
-	realConfPath, err := loadRealPath(d.dictPath)
+func main() {
+	r := gin.Default()
+	d := dict.NewDict("dict.yaml")
+	err := d.Load()
 	if err != nil {
-		log.Fatalf("Get real conf path failed: %#v", err)
-		return err
+		fmt.Println("Load dict failed")
+		return
 	}
-	yamlFile, err := ioutil.ReadFile(realConfPath)
-	if err != nil {
-		log.Fatalf("Read file failed: %#v", err)
-		return err
-	}
-	err = yaml.Unmarshal(yamlFile, &d.wordList)
-	if err != nil {
-		log.Fatalf("Unmarshal failed: %#v", err)
-		return err
-	}
-	return nil
-}
-
-// Size returns the size of a dict
-func (d *Dict) Size() int {
-	d.size = len(d.wordList)
-	return d.size
-}
-
-// Query English names
-func (d *Dict) Query(englishName string) (string, error) {
-	for _, word := range d.wordList {
-		if word.Origin == englishName {
-			return word.Translation, nil
+	r.GET("/translate", func(c *gin.Context) {
+		entry := c.Query("entry")
+		trans, err := d.Query(entry)
+		msg := "success"
+		if err != nil {
+			trans = "error"
 		}
-	}
-	return "", errors.New("Word not found")
+		c.JSON(200, gin.H{
+			"entry":   entry,
+			"result":  trans,
+			"message": msg,
+		})
+	})
+	r.Run()
 }
